ecb7d2b0b3b30917d8f1ed9f4819af4ac6e7f879
vibindas <vibindas.m@mobme.in>
2018-04-04 13:19:18 +0530

    get the input and checking palindrome

--- ex1_check_palindrom.go
+++ ex1_check_palindrom.go
@@ -18,5 +18,12 @@ func checkPalindrome(s string) (result int) {
 }
 
 func main() {
-	fmt.Println(reverseString("hello world"))
+	var inputVal string
+	fmt.Println("Enter string:")
+	fmt.Scanf("%s\n", &inputVal)
+	if strings.Compare(inputVal, reverseString(inputVal)) != 0 {
+		fmt.Println("Entered value is not palindrome")
+	} else {
+		fmt.Println("Its a palindrome")
+	}
 }
